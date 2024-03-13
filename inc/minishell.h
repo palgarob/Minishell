047e36b421aee7798cdb9a11a516ab95e9956c41
Pedro Palomares Garcia-Obregon <pepaloma@student.42urduliz.com>
2024-03-13 18:03:08 +0100

    progress

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,14 +6,14 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:23:30 by pepaloma          #+#    #+#             */
-/*   Updated: 2024/03/13 14:28:58 by pepaloma         ###   ########.fr       */
+/*   Updated: 2024/03/13 18:01:52 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_T
 # define MINISHELL_T
 
-# include <../readline/readline.h>
+# include <readline/readline.h>
 
 # include "../libft-improved/inc/libft.h"
 
--- src/minishell.c
+++ src/minishell.c
@@ -6,14 +6,15 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:22:50 by pepaloma          #+#    #+#             */
-/*   Updated: 2024/03/13 14:29:32 by pepaloma         ###   ########.fr       */
+/*   Updated: 2024/03/13 18:02:30 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <readline/readline.h>
 #include "minishell.h"
 
 int	main(void)
 {
-	printf("hola");
+	readline("Dime: ");
 	return (0);
 }
