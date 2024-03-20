03c5a7103f2e71cd9152801a806d13cfe261c32d
Pedro Palomares <pedpalgarob@gmail.com>
2024-03-20 13:16:12 +0100

    progress

--- src/parse.c
+++ src/parse.c
@@ -6,15 +6,27 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/16 22:34:17 by pepaloma          #+#    #+#             */
-/*   Updated: 2024/03/18 15:42:15 by pepaloma         ###   ########.fr       */
+/*   Updated: 2024/03/20 13:13:34 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	syntax(char **args)
+{
+	if (**args == '|')
+		return (1);
+	if (**ft_splitlast(args) == '|')
+}
+
 int	parse(char *line)
 {
 	char	**args;
 
-	args = ft_split(line, ' ');
+	args = ft_split_shell(line, ' ');
+	if (!args)
+		return (1);
+	if (syntax(args))
+		return (perror(ERR_SYNTAX), ft_splitfree(args), 1);
+	
 }
