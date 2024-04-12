54babb77beaa14258a4b0a73c8979be2e405d8c3
Pedro Palomares Garcia-Obregon <pepaloma@student.42urduliz.com>
2024-04-12 13:00:14 +0200

    nuevas referencias a readline en la compilación

--- src/minishell.c
+++ src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: pepaloma <pepaloma@student.42urduliz.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:22:50 by pepaloma          #+#    #+#             */
-/*   Updated: 2024/04/11 21:24:39 by pepaloma         ###   ########.fr       */
+/*   Updated: 2024/04/12 12:51:31 by pepaloma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ static void	init_shell(t_shell *shell, char **environment)
 
 void	handler(__attribute__((unused)) int signum)
 {
-//	printf("\033[0K");
+//	No quiero que se imprima  ^C cuando envío la señal
 	printf("\n");
 	rl_replace_line("", 0);
 	rl_on_new_line();
@@ -61,8 +61,8 @@ static void	prompt(t_shell *shell)
 	line = readline("$ ");
 	signal(SIGINT, SIG_DFL);
 	signal(SIGQUIT, SIG_DFL);
-	if (!line) //rl_clear_history
-		exit(0);
+	if (!line)
+		exit(0); //rl_clear_history y tiene que llamar a la función ft_exit
 	if (*line)
 	{
 		add_history(line);
